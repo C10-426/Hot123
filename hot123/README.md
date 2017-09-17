7cf7ec575c559a3e49c50527d607afd45286338f
acbron <136110473@qq.com>
2017-09-17 22:16:56 +0800

    [huang_ji 2017/09/17] add layouts of management page

--- hot123/README.md
+++ hot123/README.md
@@ -1,1 +1,8 @@
-Currently nothing.
+Frontend
+1. `npm install` to install Laravel Mix
+2. `npm run dev` compile js/sass/less; `npm run production` compile minify output
+
+How to apply js/css to view
+1. write `js/css` under `resources/`
+2. add it to `webpack.mix.js`
+3. run `npm run dev` or `npm run production` in app root directory
---
+++ hot123/resources/views/layouts/manage.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>HOT123 Management</title>
+
+    <link rel="stylesheet" href="{{ URL::asset('css/management.css') }}">
+    <link href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body id="management-layout">
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <a class="navbar-brand" href="#">HOT ADMIN</a>
+        <div class="management-btn">
+            <div class="btn-group">
+                <button type="button" class="btn btn-default">Hello, username</button>
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="management-menu">
+        <ul class="list-group">
+            <li class="dropdown-header">Site</li>
+            <li class="list-group-item"><a href="#">Dashboard</a></li>
+            <li class="list-group-item"><a href="#">Lists</a></li>
+            <li role="separator" class="divider"></li>
+            <li class="dropdown-header">Advertise</li>
+            <li class="list-group-item"><a href="#">Dashboard</a></li>
+            <li class="list-group-item"><a href="#">Lists</a></li>
+            <li role="separator" class="divider"></li>
+            <li class="dropdown-header">Others</li>
+            <li class="list-group-item"><a href="#">Dashboard</a></li>
+            <li class="list-group-item"><a href="#">Lists</a></li>
+        </ul>
+    </div>
+
+    @yield('content')
+
+</body>
+</html>
