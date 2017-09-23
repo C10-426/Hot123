2802455ad2680b4c9e0209ff9d401f4b27210750
程家颖 <jiaying.cjy@alibaba-inc.com>
2017-09-23 21:44:26 +0800

    [leo][feat][model]add Site model adn create a laravel Migration for it

--- hot123/app/Site.php
+++ hot123/app/Site.php
@@ -2,49 +2,9 @@
 
 namespace App;
 
-class Site
+use Illuminate\Database\Eloquent\Model;
+
+class Site extends Model
 {
-
-    /**
-     * the name of Site
-     */
-    protected $name;
-
-    /**
-     * the link of Site
-     */
-    protected $url;
-
-    /**
-     * the description of Site
-     **/
-    protected $description;
-
-
-
-    public function fakeList() {
-        $fakeSites = array();
-        for($x = 0; $x <= 1; $x++) {
-            $site = new Site();
-            $site->name = "site{$x}";
-            $site->url = "http://php.net/manual/zh/language.types.object.php";
-            $site->description = "I'm a test site{$x}";
-            $fakeSites[] = $site;
-        }
-        print_r($fakeSites);
-
-        foreach($fakeSites as $s) {
-            echo $s->name."\n";
-        }
-        return $fakeSites;
-    }
-
-    public function toString() {
-        return $this->name."\n".$this->url."\n".$this->description."\n\n";
-    }
-};
-
-// $site = new Site();
-// $site->fakeList();
-
-?>
+    //
+}
